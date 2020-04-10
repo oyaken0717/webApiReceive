1dfe65cfa145f8f9b13220335eb220239f7c46c9
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-04-10 18:16:47 +0900

    【modify】importの整理

--- src/main/java/com/example/controller/humanJudgeController.java
+++ src/main/java/com/example/controller/humanJudgeController.java
@@ -1,10 +1,5 @@
 package com.example.controller;
 
-//リストを実装したクラス。
-import java.util.ArrayList;
-//リスト。配列と違って格納できるデータは参照型のみ。
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
